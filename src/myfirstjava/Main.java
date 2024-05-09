ac6df3b26b1f5b9189a1e566bae3f18c831818e6
NutanMutakekar <nutanmutakekar@gmail.com>
2024-05-09 20:11:19 +0530

    commit 1

---
+++ src/myfirstjava/Main.java
@@ -0,0 +1,11 @@
+package myfirstjava;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
---
+++ src/myfirstjava/secondjava.java
@@ -0,0 +1,10 @@
+package myfirstjava;
+
+public class secondjava {
+
+	public static void main(String[] args) {
+		System.out.println("hello world2");
+
+	}
+
+}
